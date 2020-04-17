4e04bb73b2d7070668e913b50a6feb6a6805e072
dsgeek85@hotmail.com <dsgeek@hotmail.com>
2020-04-17 16:16:48 -0400

    Commit

---
+++ Preface.R
@@ -0,0 +1,47 @@
+# This is my second commit
+
+1 + 2 
+
+3 + 5 
+
+# tidyverse package includes ggplot2, tibble, tidyr, readr, purrr, and dplyr packages
+
+library(tidyverse)
+
+# Other packages needed 
+
+install.packages(c('nycflights13','gapminder','lahman'))
+
+# See if there are any updates for the packages in tidyverse
+tidverse_update()
+
+# If there is, use Tools check for package updates and select the packages that need update
+#.. or update.packages(c('',''))
+#.. or install packages again using install.packages(c('',''))
+
+# Running R code 
+
+1 + 2 
+
+# To show which package function comes from use package::function()
+# Ex: dplyr::mutate() or nycflights13::flights
+
+# Ways to get help
+ # 1 Google 
+ # 2 stackoverflow
+ # 3 reprex creation 
+    
+
+# learn R each day 
+ 
+# https://blog.rstudio.org
+
+# Online Version of book @ 
+# .. https://r4ds.had.co.nz
+
+
+# Source Code available to download @
+#.. https://github.com/hadley/r4ds 
+
+
+
